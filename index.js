5431f0ee13b3d3ddb583c0a7473cbc9205a7bd3d
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-05 14:56:36 +1000

    chef done sending

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
 
+const chef = require("./data/chef.json");
+
 app.get("/", (req, res) => {
 	res.send("Khabar khabo");
+});
+
+app.get("/chefs", (req, res) => {
+	res.send(chef);
 });
 
 app.listen(port, () => {
