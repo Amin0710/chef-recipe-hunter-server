f086285706c1d218ea4970ab42be9ed45dd795b1
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-05 17:12:28 +1000

    image url fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 
 const chef = require("./data/chef.json");
 
+app.use(cors());
 app.get("/", (req, res) => {
 	res.send("Khabar khabo");
 });
