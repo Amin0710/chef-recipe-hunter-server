d3274c4824d3d004ea25bd81aa55994ea0fe992f
Nurul Amin <nurul.amin.naim@gmail.com>
2023-05-05 17:27:09 +1000

    recipes ready

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5001;
 
-const chef = require("./data/chef.json");
+const chefs = require("./data/chef.json");
+const recipes = require("./data/recipes.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -11,7 +12,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/chefs", (req, res) => {
-	res.send(chef);
+	res.send(chefs);
+});
+app.get("/recipes", (req, res) => {
+	res.send(recipes);
 });
 
 app.listen(port, () => {
